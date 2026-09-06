82d58daabef0da8bb39656a66f5b282cc19e8e2f
shree <shreerajgudade124@gmail.com>
2026-09-06 23:12:35 +0530

    dark pdf

--- components/providers.tsx
+++ components/providers.tsx
@@ -20,7 +20,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
     return (
         <SessionProvider>
             <QueryClientProvider client={queryClient}>
-                <ThemeProvider attribute="class" defaultTheme="dark" enableSystem={false}>
+                <ThemeProvider attribute="class" defaultTheme="system" enableSystem={true}>
                     <TooltipProvider>
                         {children}
                         <Toaster
